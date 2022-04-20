85d37dd8423642cb667842a93fdbff1267847747
timmellis <tim.m.ellis@gmail.com>
2022-04-20 11:31:29 -0400

    converted feed page to use Post component

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -6,8 +6,8 @@ const Post = ({post}) => {
     return (
         <div className='post-card-container' key={post.id}>
             <Link to={`/details/${post.id}`} >
-                <img src={post.image} style={{maxWidth: '400px'}} alt='post img' />
-                <p>{post.description}</p>
+                <img src={post.image} alt='post img' />
+                <p>{post.title}</p>
             </Link>
             <button 
                     onClick={() => {DeletePost(post.id)

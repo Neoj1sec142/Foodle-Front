3b8249dcee606c473d14aa18c168d9b30f7d8b22
timmellis <tim.m.ellis@gmail.com>
2022-04-20 11:35:02 -0400

    edited post component nav link

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,14 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { DeletePost } from '../services/PostServices'
 
 const Post = ({post}) => {
+const navigate = useNavigate();
 
     return (
-        <div className='post-card-container' key={post.id}>
-            <Link to={`/details/${post.id}`} >
+        <div className='post-card-container' key={post.id} onClick={() => navigate(`/details/${post.id}`)}>
                 <img src={post.image} alt='post img' />
                 <p>{post.title}</p>
-            </Link>
             <button 
                     onClick={() => {DeletePost(post.id)
                     window.top.location.reload(true)}}

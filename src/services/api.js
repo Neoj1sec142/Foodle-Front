2aefa02d49f3ad0fe772f2501e77369dc9b9c160
Mark Harmon <neothej1sec@gmail.com>
2022-04-21 14:49:41 -0500

    changed api.js base_url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://foodle-back.herokuapp.com' : 'http://localhost:3001'
-
+// export const BASE_URL = 'http://localhost:3001' 
+export const BASE_URL = 'https://foodle-back.herokuapp.com' 
 const Client = Axios.create({ baseURL: BASE_URL })
 
 //intercepts every request axios makes

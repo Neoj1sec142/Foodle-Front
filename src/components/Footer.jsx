03881f18ee3501ac71181fead93577dc3d7044e4
timmellis <tim.m.ellis@gmail.com>
2022-04-21 22:26:30 -0400

    added joshuas changes for UserSearch styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+const Footer = () => {
+  const navigate = useNavigate()
+  return (
+    <footer>
+    </footer>
+  )
+}
+
+export default Footer
